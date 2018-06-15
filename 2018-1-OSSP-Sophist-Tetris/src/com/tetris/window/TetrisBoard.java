3f4dd5ea38cea0529fa66784090a4bd149d04dc5
taegun <taegun89@hotmail.com>
2018-06-15 18:21:04 +0900

    리무브 라인 재수정

--- 2018-1-OSSP-Sophist-Tetris/src/com/tetris/window/TetrisBoard.java
+++ 2018-1-OSSP-Sophist-Tetris/src/com/tetris/window/TetrisBoard.java
@@ -1336,7 +1336,6 @@ public class TetrisBoard extends JPanel implements Runnable, KeyListener, MouseL
 						itemClearLineIndex = j;
 						System.out.println("Clear#" + itemClearLineNumber);
 						blockList.remove(s);
-						s--;
 						isClear = true;
 					}
 					else if(map[lineNumber][j].color.equals(new Color(255,0,255))) {
@@ -1344,12 +1343,11 @@ public class TetrisBoard extends JPanel implements Runnable, KeyListener, MouseL
 						itemBlindLineIndex = j;
 						System.out.println("Blind#" + itemBlindLineNumber);
 						blockList.remove(s);
-						s--;
 						isBlind = true;
 					}
 					else {
 						blockList.remove(s);
-						s--;
+						
 						EXP_SOUND = true;
 					}
 				}
